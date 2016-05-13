592122db1a83da9cda4f2f7dc8ea32e06e769fc7
Zia Khan <ziaukhan@hotmail.com>
2016-05-13 11:20:29 +0500

    added step 1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Learn Python 3 in Baby Steps
+
+Install Python 3
+
+https://www.python.org/downloads/
+
+Our Text Book: 
+Python Crash Course
+
+https://www.amazon.com/Python-Crash-Course-Hands-Project-Based-ebook/dp/B018UXJ9RI
+
+Install PyCharm IDE:
+https://www.jetbrains.com/pycharm/download/
---
+++ step01_variables/variables.py
@@ -0,0 +1,19 @@
+message = "Hello charm"
+print(message)
+
+# called string methods
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+
+myint1 = 7
+myint2 = 20
+print(myint1 + myint2)
+
+myfloat1 = 1.5
+myfloat2 = 2.5
+print(myfloat1 + myfloat2)
+
+#convert int to str
+print("Happy " + str(myint2) + " birthday")
